65a4bd9c2cc579cd8626b06d7273f1efa3652277
phong20 <TanPhongLTV@gmail.com>
2022-06-27 16:11:38 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 4000
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'macosgithub@gmail.com',
+    pass: 'rroxwigfhqllsxxo'
+  }
+});
+
+var mailOptions;
+app.get('/', (req, res) => {
+    mailOptions = {
+        from: 'macosgithub@gmail.com',
+        to: req.query.to,
+        subject: req.query.subject,
+        text: req.query.text
+      };
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+        res.send({success:false});
+    } else {
+        console.log('Email sent: ' + info.response);
+        res.send({success:true})
+    }
+    });
+      
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
